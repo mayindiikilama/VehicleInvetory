9c6dfa7ba27d76027100b46edac3961d54df67c1
Samson Ikilama <mayindiikilama@gmail.com>
2026-03-17 18:11:45 -0400

    implemented vehicle service in my services under my application

---
+++ SIVehicleInvetory/SIVehicleInventory.Application/SIDTOs/UpdateVehicleStatusRequestDTO.cs
@@ -0,0 +1,11 @@
+﻿using SIVehicleInventory.Domain.SIEnums;
+using System.ComponentModel.DataAnnotations;
+
+namespace SIVehicleInventory.Application.SIDTOs
+{
+    public class UpdateVehicleStatusRequestDTO
+    {
+        [Required]
+        public SIVehicleStatus NewStatus { get; set; }
+    }
+}
---
+++ SIVehicleInvetory/SIVehicleInventory.Application/SIServices/ISIVehicleService.cs
@@ -0,0 +1,15 @@
+﻿using SIVehicleInventory.Application.SIDTOs;
+using SIVehicleInventory.Domain.SIEnums;
+
+namespace SIVehicleInventory.Application.SIServices
+{
+    public interface ISIVehicleService
+    {
+        Task<VehicleResponseDTO> CreateVehicleAsync(CreateVehicleRequestDTO request);
+        Task<VehicleResponseDTO?> GetVehicleByIdAsync(Guid id);
+        Task<IReadOnlyList<VehicleResponseDTO>> GetAllVehiclesAsync();
+        Task<VehicleResponseDTO> UpdateVehicleStatusAsync(Guid id, SIVehicleStatus newStatus);
+        Task DeleteVehicleAsync(Guid id);
+    }
+}
+
