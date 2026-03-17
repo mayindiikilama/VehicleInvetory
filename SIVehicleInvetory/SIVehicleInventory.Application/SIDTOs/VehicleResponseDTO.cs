59fdfd5b99a5488b1c60bc9c7d0d0fe9ff174ece
Samson Ikilama <mayindiikilama@gmail.com>
2026-03-17 17:53:05 -0400

    Creation of VehicleResponseDTO

---
+++ SIVehicleInvetory/SIVehicleInventory.Application/SIDTOs/VehicleResponseDTO.cs
@@ -0,0 +1,13 @@
+﻿using SIVehicleInventory.Domain.SIEnums;
+
+namespace SIVehicleInventory.Application.SIDTOs
+{
+    public class VehicleResponseDTO
+    {
+        public Guid Id { get; set; }
+        public string VehicleCode { get; set; } = null!;
+        public Guid LocationId { get; set; }
+        public string VehicleType { get; set; } = null!;
+        public SIVehicleStatus Status { get; set; }
+    }
+}
